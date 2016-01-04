3ce626aac75ab6a0f143ddcd54f4bd86b636d353
Jie Wang <wjidea@gmail.com>
2016-01-04 17:36:51 -0500

    update diff plot with latticExtra

--- Sandbox.R
+++ Sandbox.R
@@ -44,6 +44,8 @@ EC50SumFilterPlate[!(EC50SumFilterPlate$strainName %in% EC50SumPlateGeoYear$stra
 
 EC50SumConidiaGeoYear[!(EC50SumConidiaGeoYear$strainName %in% EC50SumFilterConidia$strainName),]
 EC50SumFilterConidia[!(EC50SumFilterConidia$strainName %in% EC50SumConidiaGeoYear$strainName),]
+
+
 # Function to calculate accurate EC50 values
 accuEC <- function(data, method = c(),...){
   
@@ -51,4 +53,89 @@ accuEC <- function(data, method = c(),...){
 }
 
 
-# Test code
+
+# correct strain name
+EC50SumConidiaGeoYear$strainName[EC50SumConidiaGeoYear$strainName == "Mont-1"] <- "Mont1"
+# merge two EC50 df
+plateEC50Simple <- select(EC50SumPlateGeoYear, strainName, EC50, StdErr, 
+                          Lower, Upper) %>% rename(plateEC50 = EC50)
+conidiaEC50Simple <- select(EC50SumConidiaGeoYear, strainName, EC50, StdErr, 
+                          Lower, Upper) %>% rename(conidiaEC50 = EC50)
+dupNamePlate <- unique(plateEC50Simple$strainName[duplicated(plateEC50Simple$strainName)])
+dupNameConidia <- unique(conidiaEC50Simple$strainName[duplicated(conidiaEC50Simple$strainName)])
+
+plateEC50SimpleUniq <- plateEC50Simple[!plateEC50Simple$strainName %in% dupNamePlate,]
+conidiaEC50SimpleUniq <- conidiaEC50Simple[!conidiaEC50Simple$strainName %in% dupNameConidia,]
+
+plateEC50SimpleDupl  <- 
+  plateEC50Simple[plateEC50Simple$strainName %in% dupNamePlate,] %>% 
+  group_by(strainName) %>% 
+  summarise(plateECMean = mean(plateEC50), 
+            StdErr = sd(plateEC50) / sqrt(length(plateEC50)),
+            Lower = plateECMean - StdErr,
+            Upper = plateECMean + StdErr) %>%
+  rename(plateEC50 = plateECMean)
+  
+conidiaEC50SimpleDupl <- 
+  conidiaEC50Simple[conidiaEC50Simple$strainName %in% dupNameConidia,] %>% 
+  group_by(strainName) %>% 
+  summarise(conidiaECMean = mean(conidiaEC50), 
+            StdErr = sd(conidiaEC50) / sqrt(length(conidiaEC50)),
+            Lower = conidiaECMean - StdErr,
+            Upper = conidiaECMean + StdErr) %>%
+  rename(conidiaEC50 = conidiaECMean)
+
+plateEC50 <- rbind(plateEC50SimpleUniq, plateEC50SimpleDupl) %>%
+  rename(pEC50 = plateEC50, pSE = StdErr, pL = Lower, pU = Upper)
+conidiaEC50 <- rbind(conidiaEC50SimpleUniq, conidiaEC50SimpleDupl) %>%
+  rename(cEC50 = conidiaEC50, cSE = StdErr, cL = Lower, cU = Upper)
+rownames(plateEC50) <- 1:length(rownames(plateEC50))
+rownames(conidiaEC50) <- 1:length(rownames(conidiaEC50))
+
+compStrainsNames <- merge(plateEC50, conidiaEC50, by = "strainName")$strainName
+compPdata <- plateEC50[plateEC50$strainName %in% compStrainsNames, ]
+compPdata$assay <- "plate"
+colnames(compPdata) <- c("strainName", "EC50", "SE", "Lower", "Upper", "assay")
+compCdata <- conidiaEC50[conidiaEC50$strainName %in% compStrainsNames,]
+compCdata$assay <- "conidia"
+colnames(compCdata) <- c("strainName", "EC50", "SE", "Lower", "Upper", "assay")
+compData <- rbind(compPdata, compCdata)
+
+dodge <- position_dodge(width=0.9)
+ggplot(compData, aes(x = strainName, y = EC50, fill=assay)) +
+  geom_bar(aes(fill = assay), stat="identity", 
+           position = position_dodge(width = 0.9)) +
+  geom_errorbar(aes(ymax = EC50 + SE, ymin=EC50 - SE), 
+                position = position_dodge(width = 0.9), width=0.25) +
+  scale_fill_grey(start = 0.4, end = 0.7) +
+  theme_classic() + labs(x = "Strain names", y = "EC50 estimates") +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
