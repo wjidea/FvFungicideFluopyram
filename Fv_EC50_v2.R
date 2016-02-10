db64b5aa3dc20e4131d74d69ad5b5ceb8f92b628
Jie Wang <wjidea@gmail.com>
2016-02-10 08:39:18 -0500

    update hormetic data analysis

--- Fv_EC50_v2.R
+++ Fv_EC50_v2.R
@@ -270,4 +270,38 @@ ggplot(td, aes(estimate, term, color = term)) +
   geom_vline()
 
 
+# rjags
+set.seed(1337)
+y <- rnorm(n = 20, mean = 10, sd = 5)
+mean(y)
 
+library("rjags")
+install.packages("rjags")
+# The model specification
+model_string <- "model{
+for(i in 1:length(y)) {
+y[i] ~ dnorm(mu, tau)
+}
+mu ~ dnorm(0, 0.0001)
+sigma ~ dlnorm(0, 0.0625)
+tau <- 1 / pow(sigma, 2)
+}"
+
+# Running the model
+model <- jags.model(textConnection(model_string), data = list(y = y), n.chains = 3, n.adapt= 10000)
+update(model, 10000); # Burnin for 10000 samples
+mcmc_samples <- coda.samples(model, variable.names=c("mu", "sigma"), n.iter=20000)
+plot(mcmc_samples)
+
+
+
+# BEST model
+
+library(BEST)
+y1 <- c(5.77, 5.33, 4.59, 4.33, 3.66, 4.48)
+y2 <- c(3.88, 3.55, 3.29, 2.59, 2.33, 3.59)
+priors <- list(muM = 6, muSD = 2)
+BESTout <- BESTmcmc(y1, y2, priors=priors, parallel=FALSE)
+plot(BESTout)
+plot(BESTout, compVal=1, ROPE=c(-0.1,0.1))
+plot(BESTout, which="sd")
